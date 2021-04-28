01bbb3f0995a85a760ef05fdfa7c57f840fc8b98
azesm <azesm@yahoo.com>
2021-04-28 08:59:33 +0300

    end2.3

---
+++ js/script.js
@@ -0,0 +1,53 @@
+"use strict";
+
+//console.log(4);
+//let number = 5;
+//const leftBorderWidth = 1;
+
+//number = 10;
+//console.log(number);
+
+//leftBorderWidth = 15;
+//console.log(leftBorderWidth);
+
+
+//const obj = {
+//    a: 50
+//};
+
+//obj.a = 10;
+//console.log(obj);
+
+//Простые типы - примитивы.
+
+//Числа 1,2,3
+
+//Строки 'string', 'name'
+
+//Логический тип (Boolean)
+
+//true/false
+
+//null
+
+//undefined
+
+//Symbol
+
+//BigInt
+
+
+//Объекты
+
+//Спецобъекты
+
+//Массивы []
+
+//Функции function
+
+//Объект Даты
+
+//Регулярные выражения
+
+//Ошибки
+
