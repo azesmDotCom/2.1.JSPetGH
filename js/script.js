b71c8c405c6fc14dc6826c01be8a7a318a74f63f
azesm <azesm@yahoo.com>
2021-05-31 12:54:02 +0300

    end

--- js/script.js
+++ js/script.js
@@ -22,7 +22,18 @@
 
 //Числа 1,2,3
 
+
+
+//let number = 4.6;
+
+//console.log(-4/0);
+//console.log('string' * 9);
+
+
+
 //Строки 'string', 'name'
+
+//const persone = 'Alex';
 
 //Логический тип (Boolean)
 
@@ -57,6 +68,45 @@
 //const result = confirm('Life is good?');
 //console.log(result);
 
-const answer = prompt('You older 18?', '');
-console.log(typeof(answer));
+//const answer = prompt('You older 18?', '');
+//console.log(typeof(answer));
 
+//const result = confirm(`Are you here?`);
+//console.log(result);
+
+//const answer = +prompt(`You 18 years old?`, `20`);
+//console.log(answer + 5);
+
+//const answers = [];
+
+//answers[0] = prompt(`Как ваше имя?`, ``);
+//answers[1] = prompt(`Как ваши дела?`, ``);
+//answers[2] = prompt(`How old you?`, ``);
+
+//document.write(answers);
+
+//console.log(typeof(answers));
+
+//const category = `toys`;
+
+//console.log(`https://someurl.com/S{category}/5`);
+
+//const user = 'Ivan';
+
+//alert(` Hello, ${user}`);
+
+//console.log(4 + " - object");
+
+//let incr = 10,
+//    decr = 10;
+
+//++incr;
+//--decr;
+
+//console.log(incr++);
+//console.log(decr--);
+
+const isChecked = false,
+    isClose = false;
+
+    console.log(isChecked || !isClose);
